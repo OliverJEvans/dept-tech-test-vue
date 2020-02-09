aadbece87b36f053f0879be0eaece8c9a0de30e4
Oliver Evans <ollie@shufflebeedigital.co.uk>
2020-02-09 15:43:03 +0000

    Populate autocomplete dropdown

--- src/components/AirQuality/AirQualitySearch.vue
+++ src/components/AirQuality/AirQualitySearch.vue
@@ -1,19 +1,42 @@
 <template>
   <div>
-    <div class="search__wrapper search__wrapper--open">
+    <div class="search__wrapper" v-bind:class="{ 'search__wrapper--open': searchedCity !== '' }">
       <div class="input__wrapper">
-        <input type="search" class="input" placeholder="Enter city name..." />
+        <input
+          type="search"
+          class="input"
+          placeholder="Enter city name..."
+          v-on:input="filterCities($event)"
+        />
       </div>
-      <ul class="search__results">
-        <li><button>Mace Head</button></li>
-        <li><button>Manchester</button></li>
-        <li><button>Market Harborough</button></li>
-        <li><button>Middlesbrough</button></li>
-        <li><button>Milton Keynes</button></li>
+      <ul class="search__results" v-if="searchedCity !== ''">
+        <li v-for="(city, index) in filteredCities" v-bind:key="index">
+          <button v-on:click="pinCity(city)">{{ city.name }}</button>
+        </li>
       </ul>
     </div>
   </div>
 </template>
+
+<script>
+import { mapState, mapGetters, mapActions } from 'vuex';
+
+export default {
+  computed: {
+    ...mapState({
+      searchedCity: state => state.searchedCity
+    }),
+    ...mapGetters(['filteredCities'])
+  },
+  methods: {
+    ...mapActions(['setSearchedCity', 'pinCity']),
+
+    filterCities(e) {
+      this.setSearchedCity(e.target.value);
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped>
   .input__wrapper {
@@ -35,10 +58,10 @@
     width: 100%;
     margin: 0 auto;
     border: 1px solid #b7acd8;
+    border-radius: 10px;
 
     &--open {
       background: white;
-      border-radius: 10px;
       overflow: hidden;
     }
   }
@@ -47,19 +70,25 @@
     margin: 0;
     padding: 0;
     list-style: none;
+    max-height: 185px;
+    overflow: auto;
 
     li {
       display: block;
-      padding: 10px;
 
       &:hover {
         background: #f2f2f2;
+        cursor: pointer;
       }
     }
 
     button {
+      display: block;
       background: none;
       border: none;
+      padding: 10px;
+      width: 100%;
+      text-align: left;
     }
   }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -8,20 +8,38 @@ export default new Vuex.Store({
   state: {
     cities: [],
     pinnedResults: [],
+    searchedCity: '',
+  },
+  getters: {
+    filteredCities(state) {
+      return state.cities.filter(city => city.name.toUpperCase().includes(state.searchedCity.toUpperCase()));
+    }
   },
   mutations: {
     SET_CITIES(state, payload) {
-      state.cities = payload
+      state.cities = payload;
     },
-    SET_PINNEDRESULT(state, payload) {
+    SET_PINNED_RESULT(state, payload) {
       state.pinnedResults = state.pinnedResults.push(payload);
     },
+    SET_SEARCHED_CITY(state, payload) {
+      state.searchedCity = payload;
+    }
   },
   actions: {
     getCities({ commit }) {
       axios.get('https://api.openaq.org/v1/cities?country=GB')
         .then((response) => {
           commit('SET_CITIES', response.data.results);
+        });
+    },
+    setSearchedCity({ commit }, value) {
+      commit('SET_SEARCHED_CITY', value);
+    },
+    pinCity(context, value) {
+      axios.get(`https://api.openaq.org/v1/measurements?country=GB&city=${value.name}`)
+        .then((response) => {
+          context.commit('SET_PINNED_RESULT', response.data.results)
         });
     }
   },
